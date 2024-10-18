5b4b05e76d24a69e849df89b96801d0ad82878eb
tma-hoangnhan <nhnhan1@tma.com.vn>
2024-10-18 17:55:17 +0700

    feat: implement pagination for getting menu

--- src/main/java/com/tma/restaurant_exercise_phase_2/repository/DrinkRepository.java
+++ src/main/java/com/tma/restaurant_exercise_phase_2/repository/DrinkRepository.java
@@ -1,6 +1,8 @@
 package com.tma.restaurant_exercise_phase_2.repository;
 
 import com.tma.restaurant_exercise_phase_2.model.drink.Drink;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,7 @@ import java.util.Optional;
 public interface DrinkRepository extends JpaRepository<Drink, Integer> {
     @Query("SELECT d FROM Drink d WHERE d.name = ?1")
     Optional<Drink> findDrinkByName(String name);
+
+    @Query("SELECT d FROM Drink d WHERE d.state = 1")
+    Page<Drink> getListOfActiveDrinks(Pageable pageable);
 }
--- src/main/java/com/tma/restaurant_exercise_phase_2/repository/FoodRepository.java
+++ src/main/java/com/tma/restaurant_exercise_phase_2/repository/FoodRepository.java
@@ -1,6 +1,8 @@
 package com.tma.restaurant_exercise_phase_2.repository;
 
 import com.tma.restaurant_exercise_phase_2.model.food.Food;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,7 @@ import java.util.Optional;
 public interface FoodRepository extends JpaRepository<Food, Integer> {
     @Query("SELECT f FROM Food f WHERE f.name = ?1")
     Optional<Food> findFoodByName(String name);
+
+    @Query("SELECT f FROM Food f WHERE f.state = 1")
+    Page<Food> getListOfActiveFoods(Pageable pageable);
 }
