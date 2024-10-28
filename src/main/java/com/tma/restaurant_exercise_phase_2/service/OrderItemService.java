9ece7d9d6c5f3bcecb6da25c85dc2b48d6c388f7
tma-hoangnhan <nhnhan1@tma.com.vn>
2024-10-28 15:09:56 +0700

    refactor: update Transactional lib to springframework

--- src/main/java/com/tma/restaurant_exercise_phase_2/service/OrderItemService.java
+++ src/main/java/com/tma/restaurant_exercise_phase_2/service/OrderItemService.java
@@ -9,8 +9,8 @@ import com.tma.restaurant_exercise_phase_2.repository.OrderItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Slf4j
 @Service
